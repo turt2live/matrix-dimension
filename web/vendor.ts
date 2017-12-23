1407718999398ba99a7137ba45f27ffc0128011e
Travis Ralston <travpc@gmail.com>
2017-12-22 23:19:30 -0700

    Upgrade to font awesome 5 (free)

--- web/vendor.ts
+++ web/vendor.ts
@@ -6,3 +6,9 @@ import "@angular/http";
 import "@angular/router";
 import "rxjs";
 import "@angularclass/hmr";
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import regular from '@fortawesome/fontawesome-free-regular';
+import brands from '@fortawesome/fontawesome-free-brands';
+
+fontawesome.library.add(regular, solid, brands);
