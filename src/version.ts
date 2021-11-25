62e7fcb5a11c698458dab73fcbf891d8743080bc
Tony Stipanic <tony@stipanic.ch>
2021-11-25 19:49:31 +0100

    Use package version env and only use git string in dev mode

--- src/version.ts
+++ src/version.ts
@@ -1,22 +1,20 @@
 import * as child_process from 'child_process';
 
-let version = "Unknown";
+let version = process.env.npm_package_version ? "v" + process.env.npm_package_version : "Unknown";
 let gitHash = null;
 
-try {
-    version = "v" + require("../package.json").version;
-} catch (error) {
-    // The log service isn't set up by the time we require this file
-    console.error("version", error);
-}
-
-try {
-    gitHash = child_process
-        .execSync('git rev-parse --short HEAD')
-        .toString().trim()
-} catch (error) {
-    // The log service isn't set up by the time we require this file
-    console.error("version", error);
+if (
+    process.env.NODE_ENV === "development" ||
+    process.env.TS_NODE_DEV === "true"
+) {
+    try {
+        gitHash = child_process
+            .execSync('git rev-parse --short HEAD')
+            .toString().trim()
+    } catch (error) {
+        // The log service isn't set up by the time we require this file
+        console.error("version", error);
+    }
 }
 
 export const CURRENT_VERSION = version + (gitHash ? "-" + gitHash : "");
