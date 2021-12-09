15183721c272c775e42f7d209e0db8ab7b7c4bc0
Travis Ralston <travpc@gmail.com>
2021-12-09 14:35:28 -0700

    Redecorate github integration page

--- web/app/elements/field/field.component.ts
+++ web/app/elements/field/field.component.ts
@@ -13,8 +13,19 @@ export class FieldComponent{
     @Input() label: string;
     @Input() disabled?: boolean;
     @Input() maxlength?: number;
+    @Input() asSelect?: boolean;
+    @Input() selectOptions?: ({key: string, value?: string}|string)[];
 
     public id = `${new Date().getTime()}-field-${Math.random()}`;
+
+    public get options(): {key: string, value: string}[] {
+        return this.selectOptions.map(p => {
+            if (typeof(p) === 'string') {
+                return {key: p, value: p};
+            }
+            return {key: p.key, value: p.value ?? p.key};
+        });
+    }
 
     public onValueChange() {
         this.valueChange.emit(this.value);
